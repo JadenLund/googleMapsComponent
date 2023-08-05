6aeaf10a2f36fb6ca5a47c94efb2710aaeee9de7
JadenLund <lundjaden@gmail.com>
2023-08-05 17:59:26 -0500

    test

--- googleMaps/src/App.jsx
+++ googleMaps/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import SecondComp from './components/SecondComp'
 import MyComponent from './components/MyComponent'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <MyComponent/>
+     {/* <MapWit  hADirectionsRenderer/> */}
      {/* <SecondComp/> */}
     </>
   )
--- googleMaps/src/components/MyComponent.jsx
+++ googleMaps/src/components/MyComponent.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
+import { useState } from 'react';
+import { GoogleMap, useJsApiLoader, DirectionsRenderer,  DirectionsService, MarkerF } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '800px',
   height: '400px'
 };
 
-const initialCenter = {
-  lat: 32.73124,
-  lng: -97.32594
-};
 
-const markerPosition = {
+
+const center = {
   lat: 32.73124,
   lng: -97.32594
 };
 
 function MyComponent() {
+  const [directions, setDirections] = useState(""); // Add this line
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyAOXrBGty4UmANDZbuXAy6OOvUkURjZ02o" // Replace with your API key
@@ -35,12 +34,35 @@ function MyComponent() {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
-      center={initialCenter}
+      center={center}
       zoom={15} // Adjust the initial zoom level as needed
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
-      <MarkerF  position={markerPosition} />
+      <MarkerF  position={center} />{directions && (
+      <DirectionsService
+        // Required props
+        options={{
+          destination: center,
+          origin: center,
+          travelMode: 'DRIVING', // Adjust travel mode as needed
+        }}
+        callback={(response) => {
+          if (response !== null) {
+            setDirections(response);
+          }
+        }}
+      />
+    )}
+
+    {/* Render directions on the map */}
+    {directions && (
+      <DirectionsRenderer
+        options={{
+          directions: directions,
+        }}
+      />
+    )}
     </GoogleMap>
   ) : (
     <>Map could not load</>
